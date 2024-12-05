cf73d1cf13a2855354fc586064126d5264be241c
VerticalHeretic <ls.warss98@gmail.com>
2024-12-05 19:01:36 +0100

    Changing the part 1 to compile, and doing the part 2

--- day_3/main.py
+++ day_3/main.py
@@ -1,9 +1,32 @@
 import re
 
-result = sum(
-    int(x) * int(y)
-    for line in open("input.txt")
-    for x, y in re.findall(r"mul\((\d+),(\d+)\)", line)
-)
+input = open("input.txt").read()
+instruction_pattern = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
 
+# Part I
+result = sum(
+    [
+        int(instruction.group(1)) * int(instruction.group(2))
+        for instruction in instruction_pattern.finditer(input)
+    ]
+)
+print("Part 1:")
 print(result)
+
+conditional_pattern = re.compile(r".+?(?=don?'?t?\(\)|\Z)", re.DOTALL)
+memory_parts_match = conditional_pattern.findall(input)
+
+result2 = sum(
+    [
+        sum(
+            [
+                int(instruction.group(1)) * int(instruction.group(2))
+                for instruction in instruction_pattern.finditer(part)
+            ]
+        )
+        for part in memory_parts_match
+        if not part.startswith("don't()")
+    ]
+)
+print("Part 2:")
+print(result2)
